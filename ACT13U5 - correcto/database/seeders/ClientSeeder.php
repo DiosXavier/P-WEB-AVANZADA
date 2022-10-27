fc01843ac6876609bc3f444fa37f55779ab69d42
JonathanLeon2610 <jonathan2610leon@gmail.com>
2022-10-27 09:14:37 -0600

    Trabajo correcto

---
+++ ACT13U5 - correcto/database/seeders/ClientSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Client;
+
+class ClientSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        /*  */
+        $client = new Client();
+        $client->name = 'Xavier';
+        $client->email = 'jeju@uabcs.com';
+        $client->phone_number = '5556126129';
+        $client->save();
+
+
+        $client = new Client();
+        $client->name = 'XD';
+        $client->phone_number = '6969696969';
+        $client->email = 'urmmisgy.com';
+        $client->save();
+
+
+    }
+}
